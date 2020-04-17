8d4057f57752b50341a7d7dc85a2dfa54053990d
graves501 <graves501@protonmail.com>
2020-04-17 21:23:12 +0200

    Chore: Extract string variables to constants, add Slf4j for logging, formatting

--- src/main/java/io/github/graves501/chucknorrisgreeter/PlayerEventListener.java
+++ src/main/java/io/github/graves501/chucknorrisgreeter/PlayerEventListener.java
@@ -5,15 +5,21 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-@Log
+@Slf4j
 public class PlayerEventListener implements Listener {
+
+    private static final String CHUCK_NORRIS_API_CURL_REQUEST
+        = "curl -s https://api.chucknorris.io/jokes/random";
+    private static final String WHITE_SPACE_SEPARATOR = " ";
+    private static final String VALUE = "value";
+    private static final String JOKE_PREFIX = ChatColor.RED + "[Chuck Norris Joke] ";
 
     @EventHandler
     private void onPlayerJoin(final PlayerJoinEvent playerJoinEvent) throws Exception {
@@ -28,22 +34,22 @@ public class PlayerEventListener implements Listener {
 
     private String requestJokeAndParseResponse() throws IOException {
 
-        final String curlRequest = "curl -s https://api.chucknorris.io/jokes/random";
-        ProcessBuilder processBuilder = new ProcessBuilder(curlRequest.split(" "));
+        ProcessBuilder processBuilder = new ProcessBuilder(CHUCK_NORRIS_API_CURL_REQUEST.split(
+            WHITE_SPACE_SEPARATOR));
         final Process process = processBuilder.start();
 
         JsonElement jsonRootElement = new JsonParser()
             .parse(new InputStreamReader(process.getInputStream()));
         JsonObject jsonObject = jsonRootElement.getAsJsonObject();
 
-        final String receivedJoke = jsonObject.get("value").getAsString();
-        log.info("Received joke: " + receivedJoke);
+        final String receivedJoke = jsonObject.get(VALUE).getAsString();
+
+        log.info("Received joke: {}", receivedJoke);
 
         return receivedJoke;
     }
 
     private String formatJokeResponse(final String joke) {
-        final String JOKE_PREFIX = ChatColor.RED + "[Chuck Norris Joke] ";
         return JOKE_PREFIX + ChatColor.WHITE + joke;
     }
 }
