04101742c8e2d85fbb5d3e1da46cb922ce73a0ca
graves501 <andrewsparr@hotmail.com>
2020-12-29 23:32:43 +0100

    Chore: Minor refactoring, added apache dependency for stringutils

--- src/main/java/io/github/graves501/chucknorrisgreeter/PlayerEventListener.java
+++ src/main/java/io/github/graves501/chucknorrisgreeter/PlayerEventListener.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonParser;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -15,41 +16,37 @@ import org.bukkit.event.player.PlayerJoinEvent;
 @Slf4j
 public class PlayerEventListener implements Listener {
 
-    private static final String CHUCK_NORRIS_API_CURL_REQUEST
-        = "curl -s https://api.chucknorris.io/jokes/random";
-    private static final String WHITE_SPACE_SEPARATOR = " ";
+    private static final String CHUCK_NORRIS_API = "https://api.chucknorris.io/jokes/random";
+    private static final String CHUCK_NORRIS_API_CURL_REQUEST = "curl -s " + CHUCK_NORRIS_API;
     private static final String VALUE = "value";
-    private static final String JOKE_PREFIX = ChatColor.RED + "[Chuck Norris Joke] ";
+    private static final String CHUCK_NORRIS_JOKE_PREFIX = ChatColor.RED + "[Chuck Norris Joke] ";
 
     @EventHandler
-    private void onPlayerJoin(final PlayerJoinEvent playerJoinEvent) throws Exception {
-        Bukkit.broadcastMessage(getJoke());
+    private void onPlayerJoin(final PlayerJoinEvent playerJoinEvent) throws IOException {
+        Bukkit.broadcastMessage(getChuckNorrisJoke());
     }
 
-    private String getJoke() throws Exception {
-
-        final String jokeResponse = requestJokeAndParseResponse();
-        return formatJokeResponse(jokeResponse);
+    private String getChuckNorrisJoke() throws IOException {
+        final String receivedChuckNorrisJoke = requestJokeAndParseResponse();
+        return formatReceivedChuckJoke(receivedChuckNorrisJoke);
     }
 
     private String requestJokeAndParseResponse() throws IOException {
+        ProcessBuilder jokeRequestProcessBuilder = new ProcessBuilder(
+            CHUCK_NORRIS_API_CURL_REQUEST.split(StringUtils.SPACE));
+        final Process jokeRequestProcess = jokeRequestProcessBuilder.start();
 
-        ProcessBuilder processBuilder = new ProcessBuilder(CHUCK_NORRIS_API_CURL_REQUEST.split(
-            WHITE_SPACE_SEPARATOR));
-        final Process process = processBuilder.start();
+        JsonElement responseJsonRootElement = new JsonParser()
+            .parse(new InputStreamReader(jokeRequestProcess.getInputStream()));
+        JsonObject responseJsonObject = responseJsonRootElement.getAsJsonObject();
 
-        JsonElement jsonRootElement = new JsonParser()
-            .parse(new InputStreamReader(process.getInputStream()));
-        JsonObject jsonObject = jsonRootElement.getAsJsonObject();
-
-        final String receivedJoke = jsonObject.get(VALUE).getAsString();
-
+        final String receivedJoke = responseJsonObject.get(VALUE).getAsString();
         log.info("Received joke: {}", receivedJoke);
 
         return receivedJoke;
     }
 
-    private String formatJokeResponse(final String joke) {
-        return JOKE_PREFIX + ChatColor.WHITE + joke;
+    private String formatReceivedChuckJoke(final String chuckNorrisJoke) {
+        return CHUCK_NORRIS_JOKE_PREFIX + ChatColor.WHITE + chuckNorrisJoke;
     }
 }
